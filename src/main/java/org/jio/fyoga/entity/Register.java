6cf209ca85088604faaf11686724c5115ed026aa
pp2506 <hoaiphuong2506@gmail.com>
2023-06-30 21:44:41 +0700

    add register entity

---
+++ src/main/java/org/jio/fyoga/entity/Register.java
@@ -0,0 +1,53 @@
+package org.jio.fyoga.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.sql.Date;
+
+@Entity
+@Getter
+@Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@AllArgsConstructor
+@NoArgsConstructor
+public class Register{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int registerID;
+
+    @Column(name = "slot_available")
+    int slotAvailable;
+
+    @Column(name = "time_available")
+    int timeAvailable;
+
+    @Column(name = "registered_date")
+    Date registeredDate;
+
+    @Column(name = "status")
+    int status;
+
+    @Column(name = "price_original")
+    float priceOriginal;
+
+    @Column(name = "price_discount")
+    float priceDiscount;
+
+    @Column(name = "slot_used")
+    int slotUsed;
+
+    @Column(name = "week_used")
+    int weekUsed;
+
+    @ManyToOne
+    @JoinColumn(name = "packageID")
+    Package packages;
+
+    @ManyToOne
+    @JoinColumn(name = "adminID")
+    Account customer;
+
+}
---
+++ src/main/java/org/jio/fyoga/model/RegisterDTO.java
@@ -0,0 +1,26 @@
+package org.jio.fyoga.model;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.sql.Date;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class RegisterDTO {
+    int registerID;
+    int slotAvailable;
+    int timeAvailable;
+    Date registeredDate;
+    int status;
+    float priceOriginal;
+    float priceDiscount;
+    int slotUsed;
+    int weekUsed;
+    int customerID;
+    int packageID;
+}
