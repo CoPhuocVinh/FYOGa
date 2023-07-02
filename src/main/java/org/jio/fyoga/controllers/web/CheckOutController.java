f85647fe5629b27d824c4554bdd45e97633337ab
CoPhuocVinh <cophuocvinh@gmail.com>
2023-07-02 18:48:32 +0700

    checkout register

--- src/main/java/org/jio/fyoga/controllers/web/CheckOutController.java
+++ src/main/java/org/jio/fyoga/controllers/web/CheckOutController.java
@@ -11,16 +11,17 @@ import jakarta.servlet.http.HttpSession;
 import org.jio.fyoga.entity.Account;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class CheckOutController {
 
     @GetMapping("/FYoGa/Course/PackageCheckOut")
-    public String checkOut(HttpSession session){
+    public String checkOut(HttpSession session, @RequestParam int packageID){
         String url = "forward:/FYoGa/Login";
         Account account = (Account) session.getAttribute("USER");
         if(account != null){
-
+            System.out.println(packageID);
 
             url = "web/checkoutCourse";
         }
