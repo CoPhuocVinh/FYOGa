36537950893d7026f34543e667806e155292d1e6
CoPhuocVinh <cophuocvinh@gmail.com>
2023-07-02 23:01:20 +0700

    update register v1

--- src/main/java/org/jio/fyoga/controllers/web/CheckOutController.java
+++ src/main/java/org/jio/fyoga/controllers/web/CheckOutController.java
@@ -9,22 +9,40 @@ package org.jio.fyoga.controllers.web;/*  Welcome to Jio word
 
 import jakarta.servlet.http.HttpSession;
 import org.jio.fyoga.entity.Account;
+import org.jio.fyoga.entity.Package;
+import org.jio.fyoga.model.PackageDTO;
+import org.jio.fyoga.service.IPackageService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Optional;
 
 @Controller
 public class CheckOutController {
 
-    @GetMapping("/FYoGa/Course/PackageCheckOut")
-    public String checkOut(HttpSession session, @RequestParam int packageID){
-        String url = "forward:/FYoGa/Login";
-        Account account = (Account) session.getAttribute("USER");
-        if(account != null){
-            System.out.println(packageID);
+    @Autowired
+    IPackageService packageService;
 
-            url = "web/checkoutCourse";
+    @GetMapping("/FYoGa/Course/PackageCheckOut")
+    public String checkOut(HttpSession session, @RequestParam int packageID, Model model){
+        String url = "web/checkoutCourse";
+        Account account = (Account) session.getAttribute("USER");
+
+        if(account == null){
+            //System.out.println(packageID);
+            url = "redirect:/FYoGa/Login";
         }
+        Optional<Package> packageEntiry = packageService.findById(packageID);
+//        PackageDTO packageDTO = new PackageDTO();
+//        BeanUtils.copyProperties(packageEntiry, packageDTO);
+//        float priceDiscount = packageDTO.getPrice() * (100 - packageDTO.getPercentDiscount())/100 ;
+//        packageDTO.setPriceDiscount(priceDiscount);
+        model.addAttribute("PAYING",packageEntiry);
+
         return url;
     }
 }
