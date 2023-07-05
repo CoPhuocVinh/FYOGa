a244ca53354b88fafaece830897077e40444d3f5
CoPhuocVinh <cophuocvinh@gmail.com>
2023-07-05 20:33:12 +0700

    checkout branch paying

---
+++ src/main/java/org/jio/fyoga/config/bibi.java
@@ -0,0 +1,11 @@
+package org.jio.fyoga.config;/*  Welcome to Jio word
+    @author: Jio
+    Date: 7/5/2023
+    Time: 8:30 PM
+    
+    ProjectName: FYoGa
+    Jio: I wish you always happy with coding <3
+*/
+
+public class bibi {
+}
