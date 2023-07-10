46b1b8307b4d1866587b81783ed169abf23d2b3c
CoPhuocVinh <cophuocvinh@gmail.com>
2023-07-10 23:10:28 +0700

    update securiry

---
+++ src/main/java/org/jio/fyoga/config/WebSecurityConfig.java
@@ -0,0 +1,11 @@
+package org.jio.fyoga.config;/*  Welcome to Jio word
+    @author: Jio
+    Date: 7/10/2023
+    Time: 11:09 PM
+    
+    ProjectName: FYoGa
+    Jio: I wish you always happy with coding <3
+*/
+
+public class WebSecurityConfig {
+}
