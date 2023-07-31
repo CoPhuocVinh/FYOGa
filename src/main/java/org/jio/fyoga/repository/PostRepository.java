376913d3374695343813d62e94ebdeefd7b96157
lehananh0606 <lehananh06@gmail.com>
2023-07-31 09:27:07 +0700

    update blog

--- src/main/java/org/jio/fyoga/repository/PostRepository.java
+++ src/main/java/org/jio/fyoga/repository/PostRepository.java
@@ -1,6 +1,7 @@
 package org.jio.fyoga.repository;
 
 
+import org.jio.fyoga.entity.Class;
 import org.jio.fyoga.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -9,5 +10,5 @@ import java.util.List;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Integer> {
-
+    List<Post> findByStatus(int status);
 }
